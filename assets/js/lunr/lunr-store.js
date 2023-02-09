6330d1ec62659d6f4049f1283604cc88a5b0eee8
Asif Hamza <hamzaasi@amazon.com>
2023-02-09 20:05:22 +0200

    Altered scss

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,31 +1,84 @@
-var store = [{
-        "title": "The mobile platform wars",
-        "excerpt":"As a child born in the 70’s, I lived through the PC platform wars of the 80’s. Although I was oblivious to what was happening at the time, I did notice some of its effects as much as any other child would. One day my dad was using WordPefect, Lotus...","categories": [],
-        "tags": ["Android","Apple","Google","iPhone","Nokia"],
-        "url": "/the-mobile-platform-wars/",
-        "teaser": null
-      },{
-        "title": "The Common Theme in Every Success Story",
-        "excerpt":"I’ve just finished reading Arnold Schwarzenegger’s biography, Total Recall: My Unbelievably True Life Story. It’s an inspirational story which chronicles his rise from rural Austria to bodybuilding champion to real-estate millionaire, movie star and Governor of California. How did he do it? By having a clear vision of what he...","categories": [],
-        "tags": ["Goals","mastery"],
-        "url": "/the-common-theme-in-every-success-story/",
-        "teaser": null
-      },{
-        "title": "How consistency allowed me to attain a 4-year old goal",
-        "excerpt":"In 2013 I was bitten by the cycling bug. It started when I was persuaded to join some friends mountain biking. They had bought expensive mountain bikes and wanted to test them out a local mountain biking trail. So I joined them with my department store special. That first ride...","categories": [],
-        "tags": [],
-        "url": "/how-consistency-allowed-me-to-attain-a-4-year-old-goal/",
-        "teaser": null
-      },{
-        "title": "Introduction - Creating a Fitness Dashboard using SwiftUI, Combine and Healthkit",
-        "excerpt":"A couple of years ago, I wrote about how I developed an app to help me be more consistent with my fitness goals. This app helped me with two main goals I had at the time - gamify my training and up my iOS Swift game. I posted some screenshots...","categories": [],
-        "tags": ["Swift","Apple","Combine","HealthKit"],
-        "url": "/Introduction-creating-a-fitness-dashboard-using-SwiftUI,Combine-and-Healthkit/",
-        "teaser": null
-      },{
-        "title": "Fitness Dashboard - Prototyping the dashboard screen",
-        "excerpt":"The first iteration of this app was my way of learning design and testing out a few technologies I was thinking of using for my day job. I taught myself Sketch and took part in the beta for new design course. That course taught me alot about how to thing...","categories": [],
-        "tags": ["Swift","Apple","Combine","HealthKit"],
-        "url": "/Fitness-Dashboard-Prototyping-the-dashboard-screen/",
-        "teaser": null
-      }]
+---
+layout: none
+---
+
+var store = [
+  {%- for c in site.collections -%}
+    {%- if forloop.last -%}
+      {%- assign l = true -%}
+    {%- endif -%}
+    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
+    {%- for doc in docs -%}
+      {%- if doc.header.teaser -%}
+        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
+      {%- else -%}
+        {%- assign teaser = site.teaser -%}
+      {%- endif -%}
+      {
+        "title": {{ doc.title | jsonify }},
+        "excerpt":
+          {%- if site.search_full_content == true -%}
+            {{ doc.content | newline_to_br |
+              replace:"<br />", " " |
+              replace:"</p>", " " |
+              replace:"</h1>", " " |
+              replace:"</h2>", " " |
+              replace:"</h3>", " " |
+              replace:"</h4>", " " |
+              replace:"</h5>", " " |
+              replace:"</h6>", " "|
+            strip_html | strip_newlines | jsonify }},
+          {%- else -%}
+            {{ doc.content | newline_to_br |
+              replace:"<br />", " " |
+              replace:"</p>", " " |
+              replace:"</h1>", " " |
+              replace:"</h2>", " " |
+              replace:"</h3>", " " |
+              replace:"</h4>", " " |
+              replace:"</h5>", " " |
+              replace:"</h6>", " "|
+            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
+          {%- endif -%}
+        "categories": {{ doc.categories | jsonify }},
+        "tags": {{ doc.tags | jsonify }},
+        "url": {{ doc.url | relative_url | jsonify }},
+        "teaser": {{ teaser | relative_url | jsonify }}
+      }{%- unless forloop.last and l -%},{%- endunless -%}
+    {%- endfor -%}
+  {%- endfor -%}{%- if site.lunr.search_within_pages -%},
+  {%- assign pages = site.pages | where_exp:'doc','doc.search != false' -%}
+  {%- for doc in pages -%}
+    {%- if forloop.last -%}
+      {%- assign l = true -%}
+    {%- endif -%}
+  {
+    "title": {{ doc.title | jsonify }},
+    "excerpt":
+        {%- if site.search_full_content == true -%}
+          {{ doc.content | newline_to_br |
+            replace:"<br />", " " |
+            replace:"</p>", " " |
+            replace:"</h1>", " " |
+            replace:"</h2>", " " |
+            replace:"</h3>", " " |
+            replace:"</h4>", " " |
+            replace:"</h5>", " " |
+            replace:"</h6>", " "|
+          strip_html | strip_newlines | jsonify }},
+        {%- else -%}
+          {{ doc.content | newline_to_br |
+            replace:"<br />", " " |
+            replace:"</p>", " " |
+            replace:"</h1>", " " |
+            replace:"</h2>", " " |
+            replace:"</h3>", " " |
+            replace:"</h4>", " " |
+            replace:"</h5>", " " |
+            replace:"</h6>", " "|
+          strip_html | strip_newlines | truncatewords: 50 | jsonify }},
+        {%- endif -%}
+      "url": {{ doc.url | absolute_url | jsonify }}
+  }{%- unless forloop.last and l -%},{%- endunless -%}
+  {%- endfor -%}
+{%- endif -%}]
